8e78e28b3fe3f1a4c9ebf91e761c168735b0afdf
Kurisu081299 <robertchristian.rosales@gmail.com>
2022-07-27 08:57:39 +0800

    added firebase to the lineup activity

---
+++ app/src/main/java/com/example/chordify/LineupInfo.kt
@@ -0,0 +1,3 @@
+package com.example.chordify
+
+data class LineupInfo(var name: String ?=null, var desc: String ?=null)
---
+++ app/src/main/java/com/example/chordify/MyAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.chordify
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private val songInfo : ArrayList<SongsInfo>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.song_item, parent, false)
+        return MyViewHolder(itemView)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val currentitem = songInfo[position]
+
+        holder.songTitle.text = currentitem.songTitle
+        holder.singer.text = currentitem.singer
+        holder.key.text = currentitem.key
+
+    }
+
+    override fun getItemCount(): Int {
+        return songInfo.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val songTitle : TextView = itemView.findViewById(R.id.tvsongtitle)
+        val singer : TextView = itemView.findViewById(R.id.tvsinger)
+        val key : TextView = itemView.findViewById(R.id.tvkey)
+
+    }
+
+
+}
